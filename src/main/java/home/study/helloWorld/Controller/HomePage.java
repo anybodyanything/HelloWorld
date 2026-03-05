c23c8258908427353cb07241a7579d69f6052380
Catarina Trancoso <catarinamftrancoso@gmail.com>
2026-03-05 10:34:11 +0000

    adds basic deployment.yaml

--- src/main/java/home/study/helloWorld/Controller/HomePage.java
+++ src/main/java/home/study/helloWorld/Controller/HomePage.java
@@ -1,7 +1,9 @@
 package home.study.helloWorld.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HomePage {
 
     @GetMapping("/")
